bd07b837cd8f799cd4901e556661db3547f1aacb
Yoshito Okada <okada@tohoku.ac.jp>
2025-04-04 04:49:51 +0000

    skip pixel format conversion if decoded format is already compatible in ROS

--- ffmpeg_image_transport/include/ffmpeg_image_transport/ffmpeg_subscriber.hpp
+++ ffmpeg_image_transport/include/ffmpeg_image_transport/ffmpeg_subscriber.hpp
@@ -69,25 +69,39 @@ protected:
               frame = std::move(sw_frame);
             }
 
-            // Configure the image converter for this frame if needed
-            if (!converter_.is_supported(frame->width, frame->height, frame.format_name(),
-                                         "bgr24")) {
-              converter_.reconfigure(frame->width, frame->height, frame.format_name(), "bgr24");
-              RCLCPP_INFO(
-                  node_->get_logger(), "Initialized converter (src: %s, dst: %s, size: %zdx%zd)",
-                  converter_.src_format_name().c_str(), converter_.dst_format_name().c_str(),
-                  converter_.width(), converter_.height());
-            }
-
-            // Make the destination image
-            // by copying the frame properties and converting the pixel data
+            // Allocate a new image message and fill properties excluding pixel data
             const auto image = std::make_shared<sensor_msgs::msg::Image>();
             image->header.stamp = fragment->header.stamp;
             image->height = frame->height;
             image->width = frame->width;
-            image->encoding = sensor_msgs::image_encodings::BGR8;
-            image->step = 3 * frame->width;
-            converter_.convert(frame, &image->data);
+
+            // Fill the pixel data, maybe converting it to a ROS-compatible format
+            if (const auto ros_encoding = to_ROS_encoding(frame->format); !ros_encoding.empty()) {
+              // If the frame format is supported in ROS, just copy the pixel data
+              image->encoding = ros_encoding;
+              image->step = frame->linesize[0];
+              const std::size_t data_size = frame->linesize[0] * frame->height;
+              image->data.resize(data_size);
+              std::memcpy(image->data.data(), frame->data[0], data_size);
+            } else {
+              // If the frame format is not supported in ROS, convert it to BGR
+
+              // Configure the image converter for this frame if needed
+              if (!converter_.is_supported(frame->width, frame->height, frame.format_name(),
+                                           "bgr24")) {
+                converter_.reconfigure(frame->width, frame->height, frame.format_name(), "bgr24");
+                RCLCPP_INFO(
+                    node_->get_logger(), "Initialized converter (src: %s, dst: %s, size: %zdx%zd)",
+                    converter_.src_format_name().c_str(), converter_.dst_format_name().c_str(),
+                    converter_.width(), converter_.height());
+              }
+
+              // Make the destination image
+              // by copying the frame properties and converting the pixel data
+              image->encoding = sensor_msgs::image_encodings::BGR8;
+              image->step = 3 * frame->width;
+              converter_.convert(frame, &image->data);
+            }
 
             // Invoke the callback with the converted image
             image_cb(image);
@@ -96,6 +110,62 @@ protected:
       }
     } catch (const std::runtime_error &error) {
       RCLCPP_ERROR(node_->get_logger(), "Error: %s", error.what());
+    }
+  }
+
+  static std::string to_ROS_encoding(const int ffmpeg_format) {
+    switch (ffmpeg_format) {
+    // RGB formats
+    case AV_PIX_FMT_RGB24:
+      return sensor_msgs::image_encodings::RGB8;
+    case AV_PIX_FMT_RGBA:
+      return sensor_msgs::image_encodings::RGBA8;
+    case AV_PIX_FMT_RGB48:
+      return sensor_msgs::image_encodings::RGB16;
+    case AV_PIX_FMT_RGBA64:
+      return sensor_msgs::image_encodings::RGBA16;
+    // BGR formats
+    case AV_PIX_FMT_BGR24:
+      return sensor_msgs::image_encodings::BGR8;
+    case AV_PIX_FMT_BGRA:
+      return sensor_msgs::image_encodings::BGRA8;
+    case AV_PIX_FMT_BGR48:
+      return sensor_msgs::image_encodings::BGR16;
+    case AV_PIX_FMT_BGRA64:
+      return sensor_msgs::image_encodings::BGRA16;
+    // Grayscale formats
+    case AV_PIX_FMT_GRAY8:
+      return sensor_msgs::image_encodings::MONO8;
+    case AV_PIX_FMT_GRAY16:
+      return sensor_msgs::image_encodings::MONO16;
+      // bayer formats
+    case AV_PIX_FMT_BAYER_RGGB8:
+      return sensor_msgs::image_encodings::BAYER_RGGB8;
+    case AV_PIX_FMT_BAYER_RGGB16:
+      return sensor_msgs::image_encodings::BAYER_RGGB16;
+    case AV_PIX_FMT_BAYER_BGGR8:
+      return sensor_msgs::image_encodings::BAYER_BGGR8;
+    case AV_PIX_FMT_BAYER_BGGR16:
+      return sensor_msgs::image_encodings::BAYER_BGGR16;
+    case AV_PIX_FMT_BAYER_GRBG8:
+      return sensor_msgs::image_encodings::BAYER_GRBG8;
+    case AV_PIX_FMT_BAYER_GRBG16:
+      return sensor_msgs::image_encodings::BAYER_GRBG16;
+    case AV_PIX_FMT_BAYER_GBRG8:
+      return sensor_msgs::image_encodings::BAYER_GBRG8;
+    case AV_PIX_FMT_BAYER_GBRG16:
+      return sensor_msgs::image_encodings::BAYER_GBRG16;
+    // YUV formats
+    case AV_PIX_FMT_UYVY422:
+      return sensor_msgs::image_encodings::UYVY;
+    case AV_PIX_FMT_YUYV422:
+      return sensor_msgs::image_encodings::YUYV;
+    case AV_PIX_FMT_NV21:
+      return sensor_msgs::image_encodings::NV21;
+    case AV_PIX_FMT_NV24:
+      return sensor_msgs::image_encodings::NV24;
+    default:
+      return "";
     }
   }
 
