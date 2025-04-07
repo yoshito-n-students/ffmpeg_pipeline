fa3f98c6fb2d2d94cee331d59fc66dedfc002133
Yoshito Okada <okada@tohoku.ac.jp>
2025-04-07 23:32:17 +0000

    refactor conversion between ffmpeg's format and ros's encoding

--- ffmpeg_cpp/src/ffmpeg_cpp.cpp
+++ ffmpeg_cpp/src/ffmpeg_cpp.cpp
@@ -30,122 +30,48 @@ std::string err2str(const int errnum) {
   return buf;
 }
 
+constexpr std::pair<AVPixelFormat, const char *> format_encoding_pairs[] = {
+    // RGB formats
+    {AV_PIX_FMT_RGB24, sensor_msgs::image_encodings::RGB8},
+    {AV_PIX_FMT_RGBA, sensor_msgs::image_encodings::RGBA8},
+    {AV_PIX_FMT_RGB48, sensor_msgs::image_encodings::RGB16},
+    {AV_PIX_FMT_RGBA64, sensor_msgs::image_encodings::RGBA16},
+    // BGR formats
+    {AV_PIX_FMT_BGR24, sensor_msgs::image_encodings::BGR8},
+    {AV_PIX_FMT_BGRA, sensor_msgs::image_encodings::BGRA8},
+    {AV_PIX_FMT_BGR48, sensor_msgs::image_encodings::BGR16},
+    {AV_PIX_FMT_BGRA64, sensor_msgs::image_encodings::BGRA16},
+    // Grayscale formats
+    {AV_PIX_FMT_GRAY8, sensor_msgs::image_encodings::MONO8},
+    {AV_PIX_FMT_GRAY16, sensor_msgs::image_encodings::MONO16},
+    // bayer formats
+    {AV_PIX_FMT_BAYER_RGGB8, sensor_msgs::image_encodings::BAYER_RGGB8},
+    {AV_PIX_FMT_BAYER_RGGB16, sensor_msgs::image_encodings::BAYER_RGGB16},
+    {AV_PIX_FMT_BAYER_BGGR8, sensor_msgs::image_encodings::BAYER_BGGR8},
+    {AV_PIX_FMT_BAYER_BGGR16, sensor_msgs::image_encodings::BAYER_BGGR16},
+    {AV_PIX_FMT_BAYER_GRBG8, sensor_msgs::image_encodings::BAYER_GRBG8},
+    {AV_PIX_FMT_BAYER_GRBG16, sensor_msgs::image_encodings::BAYER_GRBG16},
+    {AV_PIX_FMT_BAYER_GBRG8, sensor_msgs::image_encodings::BAYER_GBRG8},
+    {AV_PIX_FMT_BAYER_GBRG16, sensor_msgs::image_encodings::BAYER_GBRG16},
+    // YUV formats
+    {AV_PIX_FMT_UYVY422, sensor_msgs::image_encodings::UYVY},
+    {AV_PIX_FMT_YUYV422, sensor_msgs::image_encodings::YUYV},
+    {AV_PIX_FMT_NV21, sensor_msgs::image_encodings::NV21},
+    {AV_PIX_FMT_NV24, sensor_msgs::image_encodings::NV24}};
+
 std::string to_ros_image_encoding(const std::string &ffmpeg_format_name) {
-  switch (av_get_pix_fmt(ffmpeg_format_name.c_str())) {
-  // RGB formats
-  case AV_PIX_FMT_RGB24:
-    return sensor_msgs::image_encodings::RGB8;
-  case AV_PIX_FMT_RGBA:
-    return sensor_msgs::image_encodings::RGBA8;
-  case AV_PIX_FMT_RGB48:
-    return sensor_msgs::image_encodings::RGB16;
-  case AV_PIX_FMT_RGBA64:
-    return sensor_msgs::image_encodings::RGBA16;
-  // BGR formats
-  case AV_PIX_FMT_BGR24:
-    return sensor_msgs::image_encodings::BGR8;
-  case AV_PIX_FMT_BGRA:
-    return sensor_msgs::image_encodings::BGRA8;
-  case AV_PIX_FMT_BGR48:
-    return sensor_msgs::image_encodings::BGR16;
-  case AV_PIX_FMT_BGRA64:
-    return sensor_msgs::image_encodings::BGRA16;
-  // Grayscale formats
-  case AV_PIX_FMT_GRAY8:
-    return sensor_msgs::image_encodings::MONO8;
-  case AV_PIX_FMT_GRAY16:
-    return sensor_msgs::image_encodings::MONO16;
-  // bayer formats
-  case AV_PIX_FMT_BAYER_RGGB8:
-    return sensor_msgs::image_encodings::BAYER_RGGB8;
-  case AV_PIX_FMT_BAYER_RGGB16:
-    return sensor_msgs::image_encodings::BAYER_RGGB16;
-  case AV_PIX_FMT_BAYER_BGGR8:
-    return sensor_msgs::image_encodings::BAYER_BGGR8;
-  case AV_PIX_FMT_BAYER_BGGR16:
-    return sensor_msgs::image_encodings::BAYER_BGGR16;
-  case AV_PIX_FMT_BAYER_GRBG8:
-    return sensor_msgs::image_encodings::BAYER_GRBG8;
-  case AV_PIX_FMT_BAYER_GRBG16:
-    return sensor_msgs::image_encodings::BAYER_GRBG16;
-  case AV_PIX_FMT_BAYER_GBRG8:
-    return sensor_msgs::image_encodings::BAYER_GBRG8;
-  case AV_PIX_FMT_BAYER_GBRG16:
-    return sensor_msgs::image_encodings::BAYER_GBRG16;
-  // YUV formats
-  case AV_PIX_FMT_UYVY422:
-    return sensor_msgs::image_encodings::UYVY;
-  case AV_PIX_FMT_YUYV422:
-    return sensor_msgs::image_encodings::YUYV;
-  case AV_PIX_FMT_NV21:
-    return sensor_msgs::image_encodings::NV21;
-  case AV_PIX_FMT_NV24:
-    return sensor_msgs::image_encodings::NV24;
-  // Other formats
-  default:
-    return "";
-  }
+  const AVPixelFormat ffmpeg_format = av_get_pix_fmt(ffmpeg_format_name.c_str());
+  const auto found_it =
+      std::find_if(std::begin(format_encoding_pairs), std::end(format_encoding_pairs),
+                   [ffmpeg_format](const auto &pair) { return pair.first == ffmpeg_format; });
+  return found_it != std::end(format_encoding_pairs) ? found_it->second : "";
 }
 
 std::string to_ffmpeg_format_name(const std::string &ros_image_encoding) {
-  // RGB formats
-  if (ros_image_encoding == sensor_msgs::image_encodings::RGB8) {
-    return av_get_pix_fmt_name(AV_PIX_FMT_RGB24);
-  } else if (ros_image_encoding == sensor_msgs::image_encodings::RGBA8) {
-    return av_get_pix_fmt_name(AV_PIX_FMT_RGBA);
-  } else if (ros_image_encoding == sensor_msgs::image_encodings::RGB16) {
-    return av_get_pix_fmt_name(AV_PIX_FMT_RGB48);
-  } else if (ros_image_encoding == sensor_msgs::image_encodings::RGBA16) {
-    return av_get_pix_fmt_name(AV_PIX_FMT_RGBA64);
-  }
-  // BGR formats
-  else if (ros_image_encoding == sensor_msgs::image_encodings::BGR8) {
-    return av_get_pix_fmt_name(AV_PIX_FMT_BGR24);
-  } else if (ros_image_encoding == sensor_msgs::image_encodings::BGRA8) {
-    return av_get_pix_fmt_name(AV_PIX_FMT_BGRA);
-  } else if (ros_image_encoding == sensor_msgs::image_encodings::BGR16) {
-    return av_get_pix_fmt_name(AV_PIX_FMT_BGR48);
-  } else if (ros_image_encoding == sensor_msgs::image_encodings::BGRA16) {
-    return av_get_pix_fmt_name(AV_PIX_FMT_BGRA64);
-  }
-  // Grayscale formats
-  else if (ros_image_encoding == sensor_msgs::image_encodings::MONO8) {
-    return av_get_pix_fmt_name(AV_PIX_FMT_GRAY8);
-  } else if (ros_image_encoding == sensor_msgs::image_encodings::MONO16) {
-    return av_get_pix_fmt_name(AV_PIX_FMT_GRAY16);
-  }
-  // Bayer formats
-  else if (ros_image_encoding == sensor_msgs::image_encodings::BAYER_RGGB8) {
-    return av_get_pix_fmt_name(AV_PIX_FMT_BAYER_RGGB8);
-  } else if (ros_image_encoding == sensor_msgs::image_encodings::BAYER_RGGB16) {
-    return av_get_pix_fmt_name(AV_PIX_FMT_BAYER_RGGB16);
-  } else if (ros_image_encoding == sensor_msgs::image_encodings::BAYER_BGGR8) {
-    return av_get_pix_fmt_name(AV_PIX_FMT_BAYER_BGGR8);
-  } else if (ros_image_encoding == sensor_msgs::image_encodings::BAYER_BGGR16) {
-    return av_get_pix_fmt_name(AV_PIX_FMT_BAYER_BGGR16);
-  } else if (ros_image_encoding == sensor_msgs::image_encodings::BAYER_GRBG8) {
-    return av_get_pix_fmt_name(AV_PIX_FMT_BAYER_GRBG8);
-  } else if (ros_image_encoding == sensor_msgs::image_encodings::BAYER_GRBG16) {
-    return av_get_pix_fmt_name(AV_PIX_FMT_BAYER_GRBG16);
-  } else if (ros_image_encoding == sensor_msgs::image_encodings::BAYER_GBRG8) {
-    return av_get_pix_fmt_name(AV_PIX_FMT_BAYER_GBRG8);
-  } else if (ros_image_encoding == sensor_msgs::image_encodings::BAYER_GBRG16) {
-    return av_get_pix_fmt_name(AV_PIX_FMT_BAYER_GBRG16);
-  }
-  // YUV formats
-  else if (ros_image_encoding == sensor_msgs::image_encodings::UYVY) {
-    return av_get_pix_fmt_name(AV_PIX_FMT_UYVY422);
-  } else if (ros_image_encoding == sensor_msgs::image_encodings::YUYV) {
-    return av_get_pix_fmt_name(AV_PIX_FMT_YUYV422);
-  } else if (ros_image_encoding == sensor_msgs::image_encodings::NV21) {
-    return av_get_pix_fmt_name(AV_PIX_FMT_NV21);
-  } else if (ros_image_encoding == sensor_msgs::image_encodings::NV24) {
-    return av_get_pix_fmt_name(AV_PIX_FMT_NV24);
-  }
-  // Other formats
-  else {
-    return "";
-  }
+  const auto found_it = std::find_if(
+      std::begin(format_encoding_pairs), std::end(format_encoding_pairs),
+      [&ros_image_encoding](const auto &pair) { return pair.second == ros_image_encoding; });
+  return found_it != std::end(format_encoding_pairs) ? av_get_pix_fmt_name(found_it->first) : "";
 }
 
 // ========================================
