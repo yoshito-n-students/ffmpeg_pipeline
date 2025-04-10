fdd4dc37ad56cc30f009e94f1866cc2184937841
Yoshito Okada <okada@tohoku.ac.jp>
2025-04-10 09:39:27 +0000

    remove custom messages because they do not work well with image_transport

--- ffmpeg_cpp/include/ffmpeg_cpp/ffmpeg_cpp.hpp
+++ ffmpeg_cpp/include/ffmpeg_cpp/ffmpeg_cpp.hpp
@@ -16,9 +16,6 @@ extern "C" {
 #include <libswscale/swscale.h>
 }
 
-#include <ffmpeg_pipeline_msgs/msg/frame.hpp>
-#include <ffmpeg_pipeline_msgs/msg/packet.hpp>
-
 namespace ffmpeg_cpp {
 
 // =================
@@ -77,15 +74,10 @@ private:
 
 class Packet {
 public:
-  using Message = ffmpeg_pipeline_msgs::msg::Packet;
-
-public:
   // Allocate the packet and default the fields
   Packet();
   // Create a packet by referencing the given buffer
   Packet(const BufferRef &buf);
-
-  Message::UniquePtr to_msg(const std::string &codec_name) const;
 
   // Access to the underlying AVPacket
   AVPacket *get() { return packet_.get(); }
@@ -106,9 +98,6 @@ private:
 
 class Frame {
 public:
-  using Message = ffmpeg_pipeline_msgs::msg::Frame;
-
-public:
   // Allocate the frame and default the fields
   Frame();
 
@@ -119,8 +108,6 @@ public:
 
   // Copy the frame to the CPU-accessible memory
   Frame transfer_data() const;
-
-  Message::UniquePtr to_msg() const;
 
   // Access to the underlying AVFrame
   AVFrame *get() { return frame_.get(); }
