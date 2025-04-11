7529faa9b477269fb11a82dd069add216a3d6cb6
Yoshito Okada <okada@tohoku.ac.jp>
2025-04-11 01:48:18 +0000

    use custom msg in AudioPacketBroadcaster

--- ffmpeg_controllers/include/ffmpeg_controllers/audio_packet_broadcaster.hpp
+++ ffmpeg_controllers/include/ffmpeg_controllers/audio_packet_broadcaster.hpp
@@ -6,25 +6,24 @@
 
 #include <ffmpeg_controllers/broadcaster_base.hpp>
 #include <ffmpeg_cpp/ffmpeg_cpp.hpp>
-#include <sensor_msgs/msg/compressed_image.hpp>
+#include <ffmpeg_pipeline_msgs/msg/packet.hpp>
 
 namespace ffmpeg_controllers {
 
-// TODO: Use more appropriate message type for audio data
-class AudioPacketBroadcaster : public BroadcasterBase<sensor_msgs::msg::CompressedImage> {
+class AudioPacketBroadcaster : public BroadcasterBase<ffmpeg_pipeline_msgs::msg::Packet> {
 public:
   AudioPacketBroadcaster()
-      : BroadcasterBase<sensor_msgs::msg::CompressedImage>(/* default_sensor_name = */ "mic",
+      : BroadcasterBase<ffmpeg_pipeline_msgs::msg::Packet>(/* default_sensor_name = */ "mic",
                                                            /* topic = */ "~/packet") {}
 
 protected:
-  std::optional<sensor_msgs::msg::CompressedImage>
+  std::optional<ffmpeg_pipeline_msgs::msg::Packet>
   on_update(const rclcpp::Time & /*time*/, const rclcpp::Duration & /*period*/,
             const std::string &codec_name, const ffmpeg_cpp::Packet &packet) override {
-    sensor_msgs::msg::CompressedImage msg;
+    ffmpeg_pipeline_msgs::msg::Packet msg;
     msg.header.stamp.sec = packet->pts / 1'000'000;
     msg.header.stamp.nanosec = (packet->pts % 1'000'000) * 1'000;
-    msg.format = codec_name;
+    msg.codec = codec_name;
     msg.data.assign(packet->data, packet->data + packet->size);
     return msg;
   }
