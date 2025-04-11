c0fcaaef337a5696ba4c8ce0ec2f493efe98981f
Yoshito Okada <okada@tohoku.ac.jp>
2025-04-11 05:04:12 +0000

    rename image_transport for consistency to ffmpeg_controllers

--- ffmpeg_image_transport/src/ffmpeg_image_transport.cpp
+++ ffmpeg_image_transport/src/ffmpeg_image_transport.cpp
@@ -1,5 +1,5 @@
-#include <ffmpeg_image_transport/video_packet_subscriber.hpp>
+#include <ffmpeg_image_transport/compressed_image_subscriber.hpp>
 #include <pluginlib/class_list_macros.hpp>
 
-PLUGINLIB_EXPORT_CLASS(ffmpeg_image_transport::VideoPacketSubscriber,
+PLUGINLIB_EXPORT_CLASS(ffmpeg_image_transport::CompressedImageSubscriber,
                        image_transport::SubscriberPlugin)
