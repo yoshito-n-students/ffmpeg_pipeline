8040b5363b7dd024806226d96513ee6300fc5423
Yoshito Okada <okada@tohoku.ac.jp>
2025-04-13 12:35:42 +0000

    remove unused Input::stream_id()

--- ffmpeg_cpp/include/ffmpeg_cpp/ffmpeg_cpp.hpp
+++ ffmpeg_cpp/include/ffmpeg_cpp/ffmpeg_cpp.hpp
@@ -166,7 +166,6 @@ public:
   Input(const std::string &url, const std::string &format_name,
         const std::map<std::string, std::string> &option_map, const std::string &media_type_name);
 
-  int stream_id() const { return stream_id_; }
   std::string codec_name() const;
   CodecParameters codec_parameters() const;
 
