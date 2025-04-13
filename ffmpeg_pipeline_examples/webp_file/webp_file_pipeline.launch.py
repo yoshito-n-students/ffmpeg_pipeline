baeacd7e9d48d06dc50f04f91245b60bd949bfb1
Yoshito Okada <okada@tohoku.ac.jp>
2025-04-13 14:34:41 +0000

    format launch.py files

--- ffmpeg_pipeline_examples/webp_file/webp_file_pipeline.launch.py
+++ ffmpeg_pipeline_examples/webp_file/webp_file_pipeline.launch.py
@@ -10,47 +10,47 @@ def generate_launch_description():
     # Get URDF via xacro
     robot_description_content = Command(
         [
-            PathJoinSubstitution([FindExecutable(name="xacro")]),
-            " ",
+            PathJoinSubstitution([FindExecutable(name='xacro')]),
+            ' ',
             PathJoinSubstitution(
                 [
-                    FindPackageShare("ffmpeg_pipeline_examples"),
-                    "webp_file",
-                    "robot_description.urdf.xacro",
+                    FindPackageShare('ffmpeg_pipeline_examples'),
+                    'webp_file',
+                    'robot_description.urdf.xacro',
                 ]
             ),
         ]
     )
     robot_description = {
-        "robot_description": ParameterValue(robot_description_content, value_type=str)
+        'robot_description': ParameterValue(robot_description_content, value_type=str)
     }
 
     robot_controllers = PathJoinSubstitution(
         [
-            FindPackageShare("ffmpeg_pipeline_examples"),
-            "webp_file",
-            "controllers.yaml",
+            FindPackageShare('ffmpeg_pipeline_examples'),
+            'webp_file',
+            'controllers.yaml',
         ]
     )
 
     control_node = Node(
-        package="controller_manager",
-        executable="ros2_control_node",
+        package='controller_manager',
+        executable='ros2_control_node',
         parameters=[robot_controllers],
-        output="both",
+        output='both',
     )
 
     robot_description_pub_node = Node(
-        package="robot_state_publisher",
-        executable="robot_state_publisher",
-        output="both",
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='both',
         parameters=[robot_description],
     )
 
     compressed_image_broadcaster_spawner = Node(
-        package="controller_manager",
-        executable="spawner",
-        arguments=["compressed_image_broadcaster", "--param-file", robot_controllers],
+        package='controller_manager',
+        executable='spawner',
+        arguments=['compressed_image_broadcaster', '--param-file', robot_controllers],
     )
 
     nodes = [
