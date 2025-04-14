a2b5853b107ca5c94960e7abb2e8e1201f2f3674
Yoshito Okada <okada@tohoku.ac.jp>
2025-04-14 01:50:18 +0000

    add FrameBroadcasterBase class

--- ffmpeg_controllers/include/ffmpeg_controllers/image_broadcaster.hpp
+++ ffmpeg_controllers/include/ffmpeg_controllers/image_broadcaster.hpp
@@ -5,7 +5,7 @@
 #include <stdexcept>
 #include <string>
 
-#include <ffmpeg_controllers/packet_broadcaster_base.hpp>
+#include <ffmpeg_controllers/frame_broadcaster_base.hpp>
 #include <ffmpeg_cpp/ffmpeg_cpp.hpp>
 #include <rclcpp/logging.hpp>
 #include <sensor_msgs/image_encodings.hpp>
@@ -13,9 +13,9 @@
 
 namespace ffmpeg_controllers {
 
-class ImageBroadcaster : public PacketBroadcasterBase<sensor_msgs::msg::Image> {
+class ImageBroadcaster : public FrameBroadcasterBase<sensor_msgs::msg::Image> {
 private:
-  using Base = PacketBroadcasterBase<sensor_msgs::msg::Image>;
+  using Base = FrameBroadcasterBase<sensor_msgs::msg::Image>;
 
 public:
   ImageBroadcaster() : Base(/* default_sensor_name = */ "camera", /* topic = */ "~/image") {}
@@ -40,42 +40,8 @@ public:
 
   std::optional<Message> on_update(const rclcpp::Time & /*time*/,
                                    const rclcpp::Duration & /*period*/,
-                                   const ffmpeg_cpp::CodecParameters &codec_params,
-                                   const ffmpeg_cpp::Packet &packet) override {
+                                   const ffmpeg_cpp::Frame &frame) override {
     try {
-      // Ensure the decoder is configured for the codec
-      if (!decoder_.valid()) {
-        decoder_ = ffmpeg_cpp::Decoder(codec_params);
-        RCLCPP_INFO(get_logger(), "Configured decoder (codec: %s, hw: %s)",
-                    decoder_.codec_name().c_str(), decoder_.hw_type_name().c_str());
-      }
-
-      // Put the compressed data into the decoder
-      decoder_.send_packet(packet);
-
-      // Extract as many frames as possible from the decoder and keep only the latest frame.
-      // According to the ffmpeg's reference, there should be only one frame per video packet
-      // so no frames should be dropped.
-      ffmpeg_cpp::Frame frame;
-      while (true) {
-        ffmpeg_cpp::Frame tmp_frame = decoder_.receive_frame();
-        if (!tmp_frame.empty()) {
-          frame = std::move(tmp_frame); // Keep the latest frame
-        } else {
-          break; // No more frames available
-        }
-      }
-      if (frame.empty()) {
-        RCLCPP_WARN(get_logger(), "No frames available after decoding packet");
-        return std::nullopt;
-      }
-
-      // If the frame data is in a hardware device,
-      // transfer the data to the CPU-accessible memory before conversion
-      if (frame.is_hw_frame()) {
-        frame = frame.transfer_data();
-      }
-
       // Ensure the converter is configured for this frame
       if (!converter_.valid()) {
         converter_ = ffmpeg_cpp::Converter(frame->width, frame->height, frame.format_name(),
@@ -87,8 +53,8 @@ public:
 
       // Build the image message
       Message msg;
-      msg.header.stamp.sec = packet->pts / 1'000'000;
-      msg.header.stamp.nanosec = (packet->pts % 1'000'000) * 1'000;
+      msg.header.stamp.sec = frame->pts / 1'000'000;
+      msg.header.stamp.nanosec = (frame->pts % 1'000'000) * 1'000;
       msg.height = frame->height;
       msg.width = frame->width;
       msg.encoding = dst_encoding_;
@@ -103,8 +69,6 @@ public:
 
 protected:
   std::string dst_format_name_, dst_encoding_;
-
-  ffmpeg_cpp::Decoder decoder_;
   ffmpeg_cpp::Converter converter_;
 };
 
