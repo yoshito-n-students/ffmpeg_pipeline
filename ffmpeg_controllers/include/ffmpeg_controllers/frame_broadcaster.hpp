c66f8e1aa0c407a395a15d39ba2b690eda038ae9
Yoshito Okada <okada@tohoku.ac.jp>
2025-04-14 02:12:08 +0000

    add FrameBroadcaster publishing general frames (incomplete)

---
+++ ffmpeg_controllers/include/ffmpeg_controllers/frame_broadcaster.hpp
@@ -0,0 +1,42 @@
+#ifndef FFMPEG_CONTROLLERS_FRAME_BROADCASTER_HPP
+#define FFMPEG_CONTROLLERS_FRAME_BROADCASTER_HPP
+
+#include <optional>
+#include <stdexcept>
+#include <string>
+
+#include <ffmpeg_controllers/frame_broadcaster_base.hpp>
+#include <ffmpeg_cpp/ffmpeg_cpp.hpp>
+#include <ffmpeg_pipeline_msgs/msg/frame.hpp>
+#include <rclcpp/logging.hpp>
+
+namespace ffmpeg_controllers {
+
+class FrameBroadcaster : public FrameBroadcasterBase<ffmpeg_pipeline_msgs::msg::Frame> {
+private:
+  using Base = FrameBroadcasterBase<ffmpeg_pipeline_msgs::msg::Frame>;
+
+public:
+  FrameBroadcaster() : Base(/* default_sensor_name = */ "sensor", /* topic = */ "~/frame") {}
+
+  std::optional<Message> on_update(const rclcpp::Time & /*time*/,
+                                   const rclcpp::Duration & /*period*/,
+                                   const ffmpeg_cpp::Frame &frame) override {
+    try {
+      // Build the image message
+      Message msg;
+      msg.header.stamp.sec = frame->pts / 1'000'000;
+      msg.header.stamp.nanosec = (frame->pts % 1'000'000) * 1'000;
+      // TODO: fill data field
+      // msg.data.assign(frame->data[0], frame->linesize[0] * frame->height);
+      return msg;
+    } catch (const std::runtime_error &error) {
+      RCLCPP_ERROR(get_logger(), "Error while decoding packet: %s", error.what());
+      return std::nullopt;
+    }
+  }
+};
+
+} // namespace ffmpeg_controllers
+
+#endif
--- ffmpeg_controllers/src/ffmpeg_controllers.cpp
+++ ffmpeg_controllers/src/ffmpeg_controllers.cpp
@@ -1,10 +1,13 @@
 #include <controller_interface/controller_interface.hpp>
 #include <ffmpeg_controllers/compressed_image_broadcaster.hpp>
+#include <ffmpeg_controllers/frame_broadcaster.hpp>
 #include <ffmpeg_controllers/image_broadcaster.hpp>
 #include <ffmpeg_controllers/packet_broadcaster.hpp>
 #include <pluginlib/class_list_macros.hpp>
 
 PLUGINLIB_EXPORT_CLASS(ffmpeg_controllers::CompressedImageBroadcaster,
+                       controller_interface::ControllerInterface)
+PLUGINLIB_EXPORT_CLASS(ffmpeg_controllers::FrameBroadcaster,
                        controller_interface::ControllerInterface)
 PLUGINLIB_EXPORT_CLASS(ffmpeg_controllers::ImageBroadcaster,
                        controller_interface::ControllerInterface)
