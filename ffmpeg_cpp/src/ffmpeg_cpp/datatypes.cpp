4d01873846229b3a22b84782754c41d3689dac85
Yoshito Okada <okada@tohoku.ac.jp>
2025-04-15 23:24:10 +0000

    remove temp memo from yaml-cpp README

--- ffmpeg_cpp/src/ffmpeg_cpp/datatypes.cpp
+++ ffmpeg_cpp/src/ffmpeg_cpp/datatypes.cpp
@@ -256,28 +256,3 @@ bool convert<ffmpeg_cpp::CodecParameters>::decode(const Node &yaml,
 }
 
 } // namespace YAML
-
-#if 0
-namespace YAML {
-template <> struct convert<Vec3> {
-  static Node encode(const Vec3 &rhs) {
-    Node node;
-    node.push_back(rhs.x);
-    node.push_back(rhs.y);
-    node.push_back(rhs.z);
-    return node;
-  }
-
-  static bool decode(const Node &node, Vec3 &rhs) {
-    if (!node.IsSequence() || node.size() != 3) {
-      return false;
-    }
-
-    rhs.x = node[0].as<double>();
-    rhs.y = node[1].as<double>();
-    rhs.z = node[2].as<double>();
-    return true;
-  }
-};
-} // namespace YAML
-#endif
