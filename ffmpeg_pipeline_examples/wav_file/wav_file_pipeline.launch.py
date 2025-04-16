4cf9401cb5642460c7f4bc1eb8637b3b96b569c9
Yoshito Okada <okada@tohoku.ac.jp>
2025-04-16 18:07:41 +0800

    add wav_file example

---
+++ ffmpeg_pipeline_examples/wav_file/wav_file_pipeline.launch.py
@@ -0,0 +1,62 @@
+from launch import LaunchDescription
+from launch.substitutions import Command, FindExecutable, PathJoinSubstitution
+
+from launch_ros.actions import Node
+from launch_ros.descriptions import ParameterValue
+from launch_ros.substitutions import FindPackageShare
+
+
+def generate_launch_description():
+    # Get URDF via xacro
+    robot_description_content = Command(
+        [
+            PathJoinSubstitution([FindExecutable(name='xacro')]),
+            ' ',
+            PathJoinSubstitution(
+                [
+                    FindPackageShare('ffmpeg_pipeline_examples'),
+                    'wav_file',
+                    'robot_description.urdf.xacro',
+                ]
+            ),
+        ]
+    )
+    robot_description = {
+        'robot_description': ParameterValue(robot_description_content, value_type=str)
+    }
+
+    robot_controllers = PathJoinSubstitution(
+        [
+            FindPackageShare('ffmpeg_pipeline_examples'),
+            'wav_file',
+            'controllers.yaml',
+        ]
+    )
+
+    control_node = Node(
+        package='controller_manager',
+        executable='ros2_control_node',
+        parameters=[robot_controllers],
+        output='both',
+    )
+
+    robot_description_pub_node = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='both',
+        parameters=[robot_description],
+    )
+
+    packet_broadcaster_spawner = Node(
+        package='controller_manager',
+        executable='spawner',
+        arguments=['packet_broadcaster', '--param-file', robot_controllers],
+    )
+
+    nodes = [
+        control_node,
+        robot_description_pub_node,
+        packet_broadcaster_spawner,
+    ]
+
+    return LaunchDescription(nodes)
