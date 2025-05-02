b0bf72e9c19c212639758e5a298e0091334fe53f
Yoshito Okada <okada@tohoku.ac.jp>
2025-05-02 06:58:11 +0000

    use new Parser interface in decode_bag

--- ffmpeg_cpp/src/decode_bag.cpp
+++ ffmpeg_cpp/src/decode_bag.cpp
@@ -70,63 +70,74 @@ int main(int argc, char **argv) {
         RCLCPP_INFO(node->get_logger(), "Configured parser (codec: %s)",
                     parser.codec_names().front().c_str());
       }
-      if (!decoder.valid()) {
-        av::Dictionary options;
-        decoder = av::Decoder(comp_img->format, &options);
-        RCLCPP_INFO(node->get_logger(), "Configured decoder (codec: %s, hw: %s)",
-                    decoder.codec_name().c_str(), decoder.hw_type_name().c_str());
-      }
 
       // Copy the compressed data to the reference-counted buffer with padding
-      av::BufferRef buffer(comp_img->data.data(), comp_img->data.size());
+      av::Packet buffer(comp_img->data.data(), comp_img->data.size());
 
       // Parse the buffer and decode the compressed data
-      while (buffer.unpadded_size() > 0) {
-        // Parse the buffer and store the data in the packet
-        const av::Packet packet = parser.parse(&buffer, &decoder);
+      while (buffer->size > 0) {
+        // Parse the buffer to extract the compressed packet.
+        // Additionally, probe the codec parameters for the decoder if it has not been initialized.
+        av::Packet packet;
+        av::CodecParameters codec_params;
+        if (!decoder.valid()) {
+          std::tie(packet, codec_params) = parser.parse_initial_packet(&buffer);
+        } else {
+          packet = parser.parse_next_packet(&buffer);
+        }
+        if (packet.empty()) {
+          continue;
+        }
 
-        if (!packet.empty()) {
-          // Send the packet to the decoder
-          decoder.send_packet(packet);
-          RCLCPP_INFO(node->get_logger(), "Sent packet to the decoder: %d bytes", packet->size);
+        // Initialize the decoder with the parameters if not already done
+        if (!decoder.valid()) {
+          av::Dictionary options;
+          decoder = av::Decoder(codec_params, &options);
+          RCLCPP_INFO(node->get_logger(), "Configured decoder (%s|%s)",
+                      decoder.codec_name().c_str(), decoder.hw_type_name().c_str());
+        }
 
-          // Receive and publish the decoded frames
-          while (true) {
-            av::Frame frame = decoder.receive_frame();
-            if (frame.empty()) {
-              break; // No more frames available
-            }
-            RCLCPP_INFO(node->get_logger(), "Decoded frame: %dx%d", frame->width, frame->height);
+        // Send the packet to the decoder
+        RCLCPP_INFO(node->get_logger(), "Start decoding packet (%d bytes)", packet->size);
+        decoder.send_packet(packet);
 
-            // If the frame data is in a hardware device,
-            // transfer the data to the CPU-accessible memory before conversion
-            if (frame.is_hw_frame()) {
-              frame = frame.transfer_data();
-            }
-
-            // Initialize the image converter if not already done
-            if (!converter.valid()) {
-              converter =
-                  av::VideoConverter(frame->width, frame->height, frame.format_name(), "bgr24");
-              RCLCPP_INFO(node->get_logger(),
-                          "Configured converter (size: %zdx%zd, src: %s, dst: %s)",
-                          converter.src_width(), converter.src_height(),
-                          converter.src_format_name().c_str(), converter.dst_format_name().c_str());
-            }
-
-            // Copy the frame properties to the destination image
-            auto image = std::make_unique<sensor_msgs::msg::Image>();
-            image->header.stamp = comp_img->header.stamp;
-            image->height = frame->height;
-            image->width = frame->width;
-            image->encoding = sensor_msgs::image_encodings::BGR8;
-            image->step = 3 * frame->width;
-            image->data = converter.convert_to_vector(frame);
-
-            // Publish the destination image
-            publisher->publish(std::move(image));
-            ++n_decoded;
+        // Receive and publish the decoded frames
+        while (true) {
+          av::Frame frame = decoder.receive_frame();
+          if (frame.empty()) {
+            break; // No more frames available
           }
+          RCLCPP_INFO(node->get_logger(), "Got frame ([%s] %dx%d)", frame.format_name().c_str(),
+                      frame->width, frame->height);
+
+          // If the frame data is in a hardware device,
+          // transfer the data to the CPU-accessible memory before conversion
+          if (frame.is_hw_frame()) {
+            frame = frame.transfer_data();
+          }
+
+          // Initialize the image converter if not already done
+          if (!converter.valid()) {
+            converter =
+                av::VideoConverter(frame->width, frame->height, frame.format_name(), "bgr24");
+            RCLCPP_INFO(node->get_logger(), "Configured converter ([%s] %zdx%zd -> [%s] %zdx%zd)",
+                        converter.src_format_name().c_str(), converter.src_width(),
+                        converter.src_height(), converter.dst_format_name().c_str(),
+                        converter.dst_width(), converter.dst_height());
+          }
+
+          // Copy the frame properties to the destination image
+          auto image = std::make_unique<sensor_msgs::msg::Image>();
+          image->header.stamp = comp_img->header.stamp;
+          image->height = frame->height;
+          image->width = frame->width;
+          image->encoding = sensor_msgs::image_encodings::BGR8;
+          image->step = 3 * frame->width;
+          image->data = converter.convert_to_vector(frame);
+
+          // Publish the destination image
+          publisher->publish(std::move(image));
+          ++n_decoded;
         }
       }
     } catch (const std::runtime_error &error) {
