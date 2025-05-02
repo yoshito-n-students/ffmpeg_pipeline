d7eada7bb20fcd1bda352d36e911598918245531
Yoshito Okada <okada@tohoku.ac.jp>
2025-05-02 23:31:02 +0000

    write CodecParameters as well as Packet in PacketWriter

--- ffmpeg_controllers/include/ffmpeg_controllers/packet_writer.hpp
+++ ffmpeg_controllers/include/ffmpeg_controllers/packet_writer.hpp
@@ -8,13 +8,18 @@
 
 namespace ffmpeg_controllers {
 
-class PacketWriter : public ControllerBase<input_options::Read<ffmpeg_cpp::Packet>,
-                                           output_options::Write<ffmpeg_cpp::Packet>> {
+class PacketWriter
+    : public ControllerBase<std::tuple<input_options::Read<ffmpeg_cpp::Packet>,
+                                       input_options::Read<ffmpeg_cpp::CodecParameters>>,
+                            std::tuple<output_options::Write<ffmpeg_cpp::Packet>,
+                                       output_options::Write<ffmpeg_cpp::CodecParameters>>> {
 protected:
-  OnGenerateReturn<output_options::Write<ffmpeg_cpp::Packet>>
+  OnGenerateReturn<std::tuple<output_options::Write<ffmpeg_cpp::Packet>,
+                              output_options::Write<ffmpeg_cpp::CodecParameters>>>
   on_generate(const rclcpp::Time & /*time*/, const rclcpp::Duration & /*period*/,
-              const ffmpeg_cpp::Packet &input_packet) override {
-    return {ControllerReturn::OK, input_packet};
+              const ffmpeg_cpp::Packet &input_packet,
+              const ffmpeg_cpp::CodecParameters &input_params) override {
+    return {ControllerReturn::OK, std::make_tuple(input_packet, input_params)};
   }
 };
 
