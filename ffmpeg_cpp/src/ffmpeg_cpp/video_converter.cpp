5a200e9001fac24c8bd52e3e30afaa94f21b80b8
Yoshito Okada <okada@tohoku.ac.jp>
2025-05-06 06:39:31 +0000

    fix bug that throws error on success from VideoConverter::create()

--- ffmpeg_cpp/src/ffmpeg_cpp/video_converter.cpp
+++ ffmpeg_cpp/src/ffmpeg_cpp/video_converter.cpp
@@ -29,7 +29,7 @@ VideoConverter VideoConverter::create(const int src_width, const int src_height,
       sws_getContext(src_width, src_height, av_get_pix_fmt(src_format_name.c_str()), //
                      dst_width, dst_height, av_get_pix_fmt(dst_format_name.c_str()), //
                      0, nullptr, nullptr, nullptr));
-  if (converter) {
+  if (!converter) {
     throw Error("VideoConverter::create(): Failed to create SwsContext");
   }
   return converter;
