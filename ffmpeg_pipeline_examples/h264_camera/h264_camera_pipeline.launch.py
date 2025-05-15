864e460e7618504eae90b9be6a1821e5c6f2facb
Yoshito Okada <okada@tohoku.ac.jp>
2025-05-15 21:39:46 +0000

    support namespace option in h264 camera example (not tested)

--- ffmpeg_pipeline_examples/h264_camera/h264_camera_pipeline.launch.py
+++ ffmpeg_pipeline_examples/h264_camera/h264_camera_pipeline.launch.py
@@ -1,14 +1,32 @@
 from launch import LaunchDescription
-from launch.substitutions import Command, FindExecutable, PathJoinSubstitution
+from launch.actions import DeclareLaunchArgument, GroupAction
+from launch.substitutions import Command, FindExecutable, LaunchConfiguration, PathJoinSubstitution
 
-from launch_ros.actions import Node
+from launch_ros.actions import Node, PushRosNamespace
 from launch_ros.descriptions import ParameterValue
 from launch_ros.substitutions import FindPackageShare
 
 
 def generate_launch_description():
-    # Get URDF via xacro
-    robot_description_content = Command(
+    ##################
+    # Launch Arguments
+    ##################
+
+    # namespace
+    declare_namespace = DeclareLaunchArgument(
+        'namespace',
+        default_value='',
+        description='Namespace for the nodes, topics, and parameters'
+    )
+    namespace = LaunchConfiguration('namespace')
+
+    #######
+    # Nodes
+    #######
+
+    # Publisher node for the hardware_description topic.
+    # It passes the contents of hardware_description.urdf.xacro as a message to ros2_control_node.
+    hw_description_content = Command(
         [
             PathJoinSubstitution([FindExecutable(name='xacro')]),
             ' ',
@@ -16,47 +34,67 @@ def generate_launch_description():
                 [
                     FindPackageShare('ffmpeg_pipeline_examples'),
                     'h264_camera',
-                    'robot_description.urdf.xacro',
+                    'hardware_description.urdf.xacro',
                 ]
             ),
         ]
     )
-    robot_description = {
-        'robot_description': ParameterValue(robot_description_content, value_type=str)
-    }
+    hw_description_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        name='hardware_description_publisher',
+        output='both',
+        parameters=[{'robot_description': ParameterValue(hw_description_content, value_type=str)}],
+        remappings=[('robot_description', 'hardware_description')],
+    )
 
-    robot_controllers = PathJoinSubstitution(
+    # The ros2_control_node.
+    # It loads the hardware plugin described in the hardware_description message
+    # and controller plugins asked from the controller spawner,
+    # and drives the hardware and controllers.
+    ffmpeg_controllers = PathJoinSubstitution(
         [
             FindPackageShare('ffmpeg_pipeline_examples'),
             'h264_camera',
             'controllers.yaml',
         ]
     )
-
-    control_node = Node(
+    ros2_control_node = Node(
         package='controller_manager',
         executable='ros2_control_node',
-        parameters=[robot_controllers],
+        name='ros2_control_node',
+        parameters=[ffmpeg_controllers],
+        remappings=[('robot_description', 'hardware_description')],
         output='both',
     )
 
-    robot_description_pub_node = Node(
-        package='robot_state_publisher',
-        executable='robot_state_publisher',
-        output='both',
-        parameters=[robot_description],
-    )
-
-    compressed_image_broadcaster_spawner = Node(
+    # The controller spawner which asks the ros2_control_node to load the controllers
+    controller_spawner = Node(
         package='controller_manager',
         executable='spawner',
-        arguments=['compressed_image_broadcaster', '--param-file', robot_controllers],
+        name='controller_spawner',
+        arguments=[
+            '--param-file',
+            ffmpeg_controllers,
+            'compressed_image_broadcaster',
+            ],
     )
 
-    nodes = [
-        control_node,
-        robot_description_pub_node,
-        compressed_image_broadcaster_spawner,
-    ]
+    ####################
+    # Launch Description
+    ####################
 
-    return LaunchDescription(nodes)
+    # Declare the launch arguments and the nodes pushed into the namespace
+    return LaunchDescription(
+        [
+            declare_namespace,
+            GroupAction(
+                [
+                    PushRosNamespace(namespace),
+                    hw_description_publisher,
+                    ros2_control_node,
+                    controller_spawner,
+                ]
+            ),
+        ]
+    )
