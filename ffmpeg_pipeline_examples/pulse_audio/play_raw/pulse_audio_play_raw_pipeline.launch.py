14c1e497de761c5ec522e903ef8e0345c8e51950
Yoshito Okada <okada@tohoku.ac.jp>
2025-05-16 01:51:23 +0000

    support namespace and input_topic options in pulse_audio examples

--- ffmpeg_pipeline_examples/pulse_audio/play_raw/pulse_audio_play_raw_pipeline.launch.py
+++ ffmpeg_pipeline_examples/pulse_audio/play_raw/pulse_audio_play_raw_pipeline.launch.py
@@ -1,14 +1,39 @@
 from launch import LaunchDescription
-from launch.substitutions import Command, FindExecutable, PathJoinSubstitution
+from launch.actions import DeclareLaunchArgument, GroupAction
+from launch.substitutions import Command, FindExecutable, LaunchConfiguration, PathJoinSubstitution
 
-from launch_ros.actions import Node
+from launch_ros.actions import Node, PushRosNamespace
 from launch_ros.descriptions import ParameterValue
 from launch_ros.substitutions import FindPackageShare
 
 
 def generate_launch_description():
-    # Get URDF via xacro
-    robot_description_content = Command(
+    ##################
+    # Launch Arguments
+    ##################
+
+    # namespace
+    declare_namespace = DeclareLaunchArgument(
+        'namespace',
+        default_value='',
+        description='Namespace for the nodes, topics, and parameters'
+    )
+    namespace = LaunchConfiguration('namespace')
+
+    # input_topic
+    declare_input_topic = DeclareLaunchArgument(
+        'input_topic',
+        default_value='packet_receiver/packet',
+        description='Topic name that packet_receiver/packet will be remapped to'
+    )
+    input_topic = LaunchConfiguration('input_topic')
+
+    #######
+    # Nodes
+    #######
+
+    # Publisher for the hardware_description topic (required by the ros2_control_node)
+    hw_description_content = Command(
         [
             PathJoinSubstitution([FindExecutable(name='xacro')]),
             ' ',
@@ -17,16 +42,22 @@ def generate_launch_description():
                     FindPackageShare('ffmpeg_pipeline_examples'),
                     'pulse_audio',
                     'play_raw',
-                    'robot_description.urdf.xacro',
+                    'hardware_description.urdf.xacro',
                 ]
             ),
         ]
     )
-    robot_description = {
-        'robot_description': ParameterValue(robot_description_content, value_type=str)
-    }
+    hw_description_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        name='hardware_description_publisher',
+        parameters=[{'robot_description': ParameterValue(hw_description_content, value_type=str)}],
+        remappings=[('robot_description', 'hardware_description')],
+        output='both',
+    )
 
-    robot_controllers = PathJoinSubstitution(
+    # The ros2_control_node
+    ffmpeg_controllers = PathJoinSubstitution(
         [
             FindPackageShare('ffmpeg_pipeline_examples'),
             'pulse_audio',
@@ -34,37 +65,46 @@ def generate_launch_description():
             'controllers.yaml',
         ]
     )
-
-    control_node = Node(
+    ros2_control_node = Node(
         package='controller_manager',
         executable='ros2_control_node',
-        parameters=[robot_controllers],
+        name='ros2_control_node',
+        parameters=[ffmpeg_controllers],
+        remappings=[
+            ('robot_description', 'hardware_description'),
+            ('packet_receiver/packet', input_topic),
+        ],
         output='both',
     )
 
-    robot_description_pub_node = Node(
-        package='robot_state_publisher',
-        executable='robot_state_publisher',
-        output='both',
-        parameters=[robot_description],
-    )
-
-    packet_controller_spawner = Node(
+    # Controller spawner
+    controller_spawner = Node(
         package='controller_manager',
         executable='spawner',
+        name='controller_spawner',
         arguments=[
+            '--controller-manager',
+            'ros2_control_node',
             '--param-file',
-            robot_controllers,
+            ffmpeg_controllers,
             '--activate-as-group',
             'packet_writer',
             'packet_receiver',
         ],
+        output='both',
     )
 
-    nodes = [
-        control_node,
-        robot_description_pub_node,
-        packet_controller_spawner,
-    ]
-
-    return LaunchDescription(nodes)
+    return LaunchDescription(
+        [
+            declare_namespace,
+            declare_input_topic,
+            GroupAction(
+                [
+                    PushRosNamespace(namespace),
+                    hw_description_publisher,
+                    ros2_control_node,
+                    controller_spawner,
+                ]
+            ),
+        ]
+    )
