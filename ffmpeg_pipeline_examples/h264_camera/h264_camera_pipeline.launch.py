820c140be79a12d128e94523f623a88855ccb216
Yoshito Okada <okada@tohoku.ac.jp>
2025-05-29 00:44:20 +0000

    fix param name in h264 example

--- ffmpeg_pipeline_examples/h264_camera/h264_camera_pipeline.launch.py
+++ ffmpeg_pipeline_examples/h264_camera/h264_camera_pipeline.launch.py
@@ -90,7 +90,7 @@ def generate_launch_description():
         name='image_view',
         parameters=[{
             'image_transport': 'ffmpeg',
-            'hw_type_name': 'cuda',  # 'vaapi', 'vulkan', ... or 'auto'
+            'compressed_image_broadcaster.image.ffmpeg.hw_type_name': 'cuda',  # 'vaapi', 'vulkan', ... or 'auto'
         }],
         remappings=[('image', 'compressed_image_broadcaster/image')],
         output='both',
